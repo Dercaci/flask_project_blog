124a72bffdcb29bcb29e70560b79e28b38b5593b
Vitalie <dercaci@gmail.com>
2021-05-28 21:31:57 +0300

    Final of the project

--- posts/forms.py
+++ posts/forms.py
@@ -3,5 +3,5 @@ from wtforms import Form, StringField, TextAreaField
 
 class PostForm(Form):
     title = StringField('Title')
-    body = TextAreaField('Body')
+    body = TextAreaField('Body') 
     
